de06b9d3f9ae9b1a8a098b845ae40b55ea911b7b
Huy Do <huyddo40@gmail.com>
2019-06-28 18:18:55 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,13 @@
 class Backer
+  attr_reader :name, :backed_projects
 
+  def initialize (name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(pro_obj)
+    @backed_projects << pro_obj
+    pro_obj.backers << self
+  end
 end
